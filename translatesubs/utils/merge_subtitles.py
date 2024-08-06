b557572d8fda812d8031b73254b0f62b0f8b3cef
xucanjie <xu.canjie@gmail.com>
2024-08-06 17:38:46 +0800

    fix

--- translatesubs/utils/merge_subtitles.py
+++ translatesubs/utils/merge_subtitles.py
@@ -4,7 +4,7 @@ import subprocess
 from typing import List
 from openai import OpenAI
 import os
-import tempfile
+
 
 def merge_subtitles(input_file, output_file):
     with open(input_file, 'r', encoding='utf-8') as f:
@@ -93,8 +93,37 @@ def remove_numbers(s):
     # 使用正则表达式匹配以数字和冒号开头的部分
     return re.sub(r'^\d+:\s*', '', s, flags=re.MULTILINE)
 
-#chatGPT
-def translate_use_chatGPT(subtitles, language='Chinese') -> List[str]:
+
+def chat_with_deepseek(system_prompt, user_prompt) -> str:
+    api_key = os.getenv("OPENAI_API_KEY")
+    client = OpenAI(api_key=api_key, base_url="https://api.deepseek.com")
+    response = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt},
+        ],
+        stream=False
+    )
+    text = response.choices[0].message.content
+    return text
+
+def chat_with_qwen2(system_prompt, user_prompt) -> str:
+    api_key = os.getenv("QWEN_OPENAI_API_KEY")
+    client = OpenAI(api_key=api_key, base_url="https://dashscope.aliyuncs.com/compatible-mode/v1")
+    response = client.chat.completions.create(
+        model="qwen-max",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt},
+        ],
+        stream=False
+    )
+    text = response.choices[0].message.content
+    return text
+
+#qwen2
+def translate_use_llm(subtitles, language='Chinese', count=2, llm='deepseek') -> List[str]:
     system_prompt = f'你是一个专业的字幕翻译员，你的任务是将以下英文字幕翻译成{language}, 你需要根据上下文进行翻译。'
     pre_text = ''
     cur_text = ''
@@ -111,49 +140,36 @@ def translate_use_chatGPT(subtitles, language='Chinese') -> List[str]:
               f"你需要将当前文本逐行按顺序翻译成{language},总共有{h+1}行，不能自行合并行，有重复的行也不能忽略， 并去掉一些特殊符合。如：-，{{\i0}}, {{\i1}}等\n每个当前文本对应一个翻译文本。\n"
               f"只返回json格式[译文1，译文2...]，译文不要显示行号，不要包含其他内容。")
     print(prompt)
-    #get api_key from env
 
-    api_key = os.getenv("OPENAI_API_KEY")
-    client = OpenAI(api_key=api_key, base_url="https://api.deepseek.com")
-
-    count = 2
     while count > 0:
-
-        response = client.chat.completions.create(
-            model="deepseek-chat",
-            messages=[
-                {"role": "system", "content": system_prompt},
-                {"role": "user", "content": prompt},
-            ],
-            stream=False
-        )
-        text = response.choices[0].message.content
-
-
+        text = ''
+        if llm == 'qwen2':
+            text = chat_with_qwen2(system_prompt, prompt)
+        if llm == 'deepseek':
+            text = chat_with_deepseek(system_prompt, prompt)
+        print(text)
         try:
-            t = re.findall(r'```json\n?(.+)\n?```', text, re.DOTALL)
-            if t[0]:
+            pattern = r"^.*?(\[.*\]).*$"
+            matches = re.findall(pattern, text, re.DOTALL)
+            print(matches)
+            if matches:
                 try:
-                    k = json.loads(t[0])
+                    k = json.loads(matches[0])
                 except:
                     count -= 1
                     continue
-                if len(k) != h+1:
+                if len(k) != h + 1:
                     print("翻译行数不对，请重新翻译")
                     count -= 1
                 return [remove_numbers(s) for s in k]
             else:
                 count -= 1
         except:
-            try:
-                t = json.loads(text)
-                if len(t) != h+1:
-                    print("翻译行数不对，请重新翻译")
-                    count -= 1
-                return [remove_numbers(s) for s in t]
-            except:
-                count -= 1
-    return []
+            count -= 1
+        count -= 1
+        return []
+
+
 
 def generate_subtitle_file(directory):
     for root, dirs, files in os.walk(directory):
@@ -172,21 +188,33 @@ def generate_subtitle_file(directory):
                             lines.append(line)
                 for i in get_batches(read_subtitles(tmpsubfile)):
                     e = i['current_batch']
-                    rs = translate_use_chatGPT(i)
+                    rs = translate_use_llm(i)
                     if len(rs) > 0 and len(rs) == len(e):
                         for index, k in enumerate(rs):
                             print(index, k)
                             try:
                                 l = e[index][0] + ',' + e[index][1] + afterstyle(k) + '\n'
-                                # l = e[index][0]+','+ k + '\n'
                                 lines.append(l)
                             except:
                                 print(index, k, e)
                             print(l)
                     else:
-                        for ori in e:
-                            l = ori[0] + ',' + ori[1] + '\n'
-                            lines.append(l)
+                        print("in qwen2---------------")
+                        rs = translate_use_llm(i, llm='qwen2')
+                        if len(rs) > 0 and len(rs) == len(e):
+                            for index, k in enumerate(rs):
+                                print(index, k)
+                                try:
+                                    l = e[index][0] + ',' + e[index][1] + afterstyle(k) + '\n'
+                                    lines.append(l)
+                                except:
+                                    print(index, k, e)
+                                print(l)
+                        else:
+                            for ori in e:
+                                l = ori[0] + ',' + ori[1] + '\n'
+                                lines.append(l)
+
                 print(lines)
                 with open(subtitle_file_path, 'w', encoding='utf-8') as f:
                     f.writelines(lines)
